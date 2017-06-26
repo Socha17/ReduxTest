608791746e92bc7315d76779f0a68706d9756e95
Matt Socha <mattsocha11@gmail.com>
2017-06-26 15:51:12 -0400

    preping redu redux store

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import {render} from 'react-dom';
+import { Provider } from 'reac-redux';
+import { createStore } from 'redux';
+import PlayerReducer from './src/reducers/players'
 import Scoreboard from './src/containers/Scoreboard';
 
 render(
